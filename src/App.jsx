1f8ed4734b89c0f229cbf7b92c3ddb79f359b5e8
Nightz <peerapat.ud@gmail.com>
2023-06-06 11:45:51 +0700

    first commit, install  tailwinds and setup config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BookIcon } from "./icons";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#232F3E]">
+        <BookIcon className="fill-[#232F3E] text-yellow-600 w-10 h-10" />
+      </div>
+    </>
+  );
+}
+
+export default App;
