961d41bb861fe5fc2b0d3441da917bf7180a8c2a
Nightz <peerapat.ud@gmail.com>
2023-06-13 10:30:24 +0700

    update redux store after remove stockslice

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import bookReducer from "./slices/bookslice";
 import cartReducer from "./slices/cartSlice";
-import stockReducer from "./slices/stockSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     book: bookReducer,
     cart: cartReducer,
-    stock: stockReducer,
   },
 });
 
