df79a00f73fc9d3d526a894d7107b2689d480586
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2023-07-13 11:53:32 +0700

    bind state on Checkoutpage OrderItem

--- src/features/checkout/components/OrderItemsSummary.jsx
+++ src/features/checkout/components/OrderItemsSummary.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import OrderItem from "./OrderItem";
 
 export default function OrderItemsSummary() {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  console.log(cartItems);
   return (
     <div>
       <div className="flow-root">
         <ul className="-my-4 divide-y divide-gray-100">
-          <OrderItem />
-          <OrderItem />
-          <OrderItem />
+          {cartItems.length > 0 &&
+            cartItems.map((item) => <OrderItem key={item.id} book={item} />)}
         </ul>
       </div>
     </div>
