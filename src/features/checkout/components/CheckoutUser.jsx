d8c541f91561a78419f8b387d1bb383adfd9f160
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2023-07-13 11:37:11 +0700

    bind state CheckoutUser

--- src/features/checkout/components/CheckoutUser.jsx
+++ src/features/checkout/components/CheckoutUser.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
+import Avatar from "../../../components/Navbar/Avatar";
+
 export default function CheckoutUser() {
+  const user = useSelector((state) => state.auth.user);
   return (
     <div className="flex items-center gap-4">
-      <span className="h-10 w-10 rounded-full bg-blue-700" />
-      <h2 className="font-medium text-gray-900">BambooYou</h2>
+      <div className="w-10">
+        <Avatar />
+      </div>
+      <h2 className="font-medium text-gray-900">{user?.username}</h2>
     </div>
   );
 }
