689e4415a8ac586bde2b9d0440bb10e1321f85df
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2023-07-14 04:54:03 +0700

    bookcard: fix typo navigate

--- src/features/book/components/Bookrow.jsx
+++ src/features/book/components/Bookrow.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
-export default function Bookrow({ title, children, nagigatePath }) {
+export default function Bookrow({ title, children, navigatePath }) {
   return (
     <div className="my-5">
       <div className="flex justify-between p-3">
         <div className="text-slate-700 font-semibold">{title}</div>
-        <Link to={nagigatePath}>
+        <Link to={navigatePath}>
           <div className="text-slate-500">see more</div>
         </Link>
       </div>
